51920733411935cd8014e87406e00c594a651067
AriDawn <arief.dwan@gmail.com>
2022-06-06 22:58:13 +1000

    Covid-19 Data Processing

--- CovidprojectQuery.sql
+++ CovidprojectQuery.sql
@@ -75,6 +75,7 @@ ORDER BY 1, 2
 
 
 -- USE CTE To Find vaccination percentage
+
 WITH PopvsVac (Location, Date, Population, new_vaccinations, total_vaccination)
 as
 (
@@ -90,3 +91,74 @@ SELECT location, MAX((total_vaccination/Population)*100) AS vaccination_percent
 FROM PopvsVac
 WHERE Population IS NOT NULL
 GROUP BY location
+
+
+--USE Temp Table
+
+DROP Table if exists #TotalVaccination
+Create Table #TotalVaccination
+(
+Location nvarchar(255), 
+Date datetime,
+Population numeric,
+New_vaccinations numeric,
+People_vaccinated numeric
+)
+
+INSERT INTO #TotalVaccination
+SELECT dea.Location, dea.Date, Population, new_vaccinations
+, SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.location ORDER BY dea.location,
+  dea.date) AS People_vaccinated
+FROM PortfolioProject_Covid..CovidDeath Dea
+JOIN PortfolioProject_Covid.. CovidVaccination Vac
+	ON Vac.location = dea.location AND Vac.date = Dea.date
+WHERE dea.continent IS NOT NULL
+ORDER BY 1, 2
+
+SELECT location, MAX((People_vaccinated/Population)*100) AS vaccination_percent
+FROM #TotalVaccination
+WHERE Population IS NOT NULL
+GROUP BY location
+
+
+--CREATE VIEW for Visualization with Tableau
+
+Create View GlobalDeath as
+SELECT SUM(new_cases) AS total_cases, SUM(cast(new_deaths as int)) as Total_deaths
+, (SUM(cast(new_deaths as int))/SUM(new_cases))*100 AS DeathPercentage
+FROM PortfolioProject_Covid..CovidDeath
+WHERE continent IS NOT NULL
+
+Create View ContinentDeath as
+SELECT location, MAX(cast(Total_deaths as int)) AS TotalDeaths
+From PortfolioProject_Covid..CovidDeath
+WHERE continent IS NULL
+AND Location Not in ('World','International','European Union')
+AND Location NOT LIKE '%income'
+GROUP BY location
+
+Create View CountriesInfected As
+SELECT Location, Population, MAX(Total_cases) AS MaxCases, MAX(ROUND((total_cases/Population)*100,2)) As PercentPopulationInfected
+From PortfolioProject_Covid..CovidDeath
+WHERE continent IS NOT NULL
+GROUP BY Location, Population
+
+DROP VIEW if exists DateCountriesInfected
+Create View DateCountriesInfected AS
+SELECT Location, date, Population, MAX(Total_cases) AS MaxCases, MAX(ROUND((total_cases/Population)*100,2)) As PercentPopulationInfected
+From PortfolioProject_Covid..CovidDeath
+WHERE continent IS NOT NULL
+GROUP BY Location, Population, date
+
+
+SELECT *
+FROM GlobalDeath
+
+SELECT *
+FROM ContinentDeath
+
+SELECT *
+FROM CountriesInfected
+
+SELECT *
+FROM DateCountriesInfected
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CovidProject
 
-Hello, this is my data analysis project about Covid. The data was found online and using Microsoft SQL
-I found some interesting data.
+Hello, this is my data analysis project about Covid, I use Microsoft SQL Server to process some of the data. The data was taken from https://ourworldindata.org/covid-deaths.
 
-I will post the visualization of the data using Tableau soon.
+I did some visualisation the data. This can be found at my tableau dashboard at
+https://public.tableau.com/app/profile/arief.darmawan7176/viz/CovidDashboard_16545182681380/Dashboard1
